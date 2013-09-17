d3206feed07cbcfa7f239c63be3a8f3eaa365859
Kate von Roeder <katevonroeder@gmail.com>
2013-09-16 17:32:32 -0700

    Daemonize

--- lib/Peppermill.rb
+++ lib/Peppermill.rb
@@ -4,22 +4,19 @@ require 'Peppermill/admin'
 require 'dotenv'
 
 
-module Peppermill
-  def self.launch
-    Dotenv.load!
-    bot = Cinch::Bot.new do
-      configure do |c|
-        c.user            = 'Peppermill'
-        c.nick            = 'Peppermill'
-        c.server          = 'irc.synirc.net'
-        c.channels        = %w(#saltybet)
-        c.plugins.plugins = [Peppermill::PepperShaker, Peppermill::Admin]
-      end
+Dotenv.load!
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.user            = 'Peppermill'
+    c.nick            = 'Peppermill'
+    c.server          = 'irc.synirc.net'
+    c.channels        = %w(#saltybet)
+    c.plugins.plugins = [Peppermill::PepperShaker, Peppermill::Admin]
+  end
 
-      on :connect do |m|
-        User('nickserv').send("identify #{ENV['PASSWORD']}")
-      end
-    end
-    bot.start
+  on :connect do |m|
+    User('nickserv').send("identify #{ENV['PASSWORD']}")
   end
 end
+bot.start
+
